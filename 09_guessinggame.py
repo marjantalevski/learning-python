2a30986cb5e53b76c62c9a1f3c70533ddc97ce9e
Marjan Talevski <marjantalevski@hotmail.com>
2018-07-20 10:41:13 +0200

    Changes, had a mistake

--- 09_guessinggame.py
+++ 09_guessinggame.py
@@ -6,7 +6,10 @@ guess = -1
 while(game == "yes"):
     try:
         counter = 0 #counter is here so it resets each new game
+        numbersrange = input("Enter an integer: ")
         a = random.randint(1, int(numbersrange)) 
+        print("Guess the generated number between 1 and " + numbersrange + ".")
+
         while guess != a:
             guess = input("Your guess: ")
             if guess == "quit":
@@ -21,6 +24,6 @@ while(game == "yes"):
             elif int(guess) < a:
                 print("Incorrect, try a larger number.")
     except:
-        print("Invalid input! Start over if you'd like.")
+       print("Invalid input! Start over if you'd like.")
     game = input("Another game - yes or no: ")
 print("Hope you had fun playing this game!")
