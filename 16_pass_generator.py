0b6c10fb25cd547d59f204a0c2c0e8653a0f7fda
Marjan Talevski <marjantalevski@hotmail.com>
2018-07-20 17:02:03 +0200

    Pass generator.

---
+++ 16_pass_generator.py
@@ -0,0 +1,16 @@
+import random
+
+def strong_pass_generator(passlength):
+    c = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    n = "01234567890"
+    s = "!@#$%^&*()?"
+    clength = passlength - 4
+    nlength = 2
+    slength = 2
+    char =  "".join(random.sample(c, clength))
+    num =  "".join(random.sample(n, nlength))
+    sym =  "".join(random.sample(s, slength))
+    password = char + num + sym
+    print(password)
+
+strong_pass_generator(10)
