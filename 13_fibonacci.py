862a738985b98555124869d9e934380fde263fe7
Marjan Talevski <marjantalevski@hotmail.com>
2018-07-20 12:10:44 +0200

    Fibonacci Generator

---
+++ 13_fibonacci.py
@@ -0,0 +1,21 @@
+def fibonacci_generator(n):
+    i = 1
+    nth = 1
+    previous_value = 0
+    store = 0
+    fib = []
+    while i <= n:
+        fib.append(nth)
+        store = previous_value
+        previous_value = nth
+        nth = store + nth
+        i += 1
+    return fib
+
+try:        
+    index = int(input("Enter the index of the last Fibonacci sequence number you want: "))
+    print(fibonacci_generator(index))
+except: 
+    print("INVALID INPUT")
+
+
